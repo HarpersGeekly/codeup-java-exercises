9c33449fbeb3d6d803e74865b380799c62a35890
HarpersGeekly <HarperRyanC@gmail.com>
2017-01-03 12:18:21 -0600

    Created repository

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by RyanHarper on 1/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Howdy, World!"); //similar to console.log
+        System.out.println("All Java code lives inside of classes");
+        System.out.println("Our program runs the 'main' method when executed");
+        System.out.println("Remember, IntelliJ will try to check for syntax");
+        System.out.println("Goodbye, World!");
+    }
+}
