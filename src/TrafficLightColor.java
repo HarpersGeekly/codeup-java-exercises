aa4d68acf7591c9f491f97d4119f104e8d799c8f
HarpersGeekly <HarperRyanC@gmail.com>
2017-01-12 17:05:46 -0600

    Created an enum called TrafficLightColor with the colors of lights as constants. In TrafficLightApp I ask for user input of the color of light and it prints out the corresponding color's action using a switch case.

---
+++ src/TrafficLightColor.java
@@ -0,0 +1,14 @@
+/**
+ * Created by RyanHarper on 1/12/17.
+ */
+
+public enum TrafficLightColor {
+    RED, YELLOW, GREEN
+}
+
+//    Playing in Traffic
+//
+//        create an enum named TrafficLightColor that contains a list of the colors that are on a stoplight
+//        create a method named with the following signature:
+//        public static void makeDrivingDecision(TrafficLightColor color)
+//        inside of the method, print out a message that tells what to do at each color light
